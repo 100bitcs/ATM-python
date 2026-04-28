cab8180ec468208e9a430adeb5d357e9e8aa4b3a
100bitcs <shobhitpainuly888@gmail.com>
2026-04-28 14:58:20 +0530

    Uploading id file

---
+++ create_id.py
@@ -0,0 +1,9 @@
+from data import id, money
+def create_id ():
+
+    id_new = int (input ('enter your id to create : '))
+    id_pass = input ('enter your password :  ')
+    id[id_new]= id_pass
+    money[id_new]= 0
+    print ("-"*40)
+
